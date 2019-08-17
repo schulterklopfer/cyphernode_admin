ac0b34c4a78ced43da0d2ef4ca54d743dddc6a8f
SKP <jash@schulterklopfer-productions.de>
2019-08-18 01:02:52 +0200

    some reformatting

--- models/user.go
+++ models/user.go
@@ -6,7 +6,7 @@ type UserModel struct {
 	gorm.Model
 	Login        string       `json:"login" gorm:"type:varchar(30);unique_index;not null"`
 	Name         string       `json:"name"`
-	Password     string       `json:"password" gorm:"type:varchar(128);not null"`
+	Password     string       `json:"password" gorm:"type:varchar(128)"`
 	EmailAddress string       `json:"emailAddress" gorm:"type:varchar(100);not null"`
 	Roles        []*RoleModel `json:"roles" gorm:"many2many:user_roles;"`
 }
