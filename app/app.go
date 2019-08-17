5285230b5feeca18929387d6074c62d3e71cdade
SKP <jash@schulterklopfer-productions.de>
2019-08-18 01:04:48 +0200

    initialise database with god mode admin user, admin role and admin app, if not existing

--- app/app.go
+++ app/app.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/schulterklopfer/cyphernode_admin/dataSource"
 	"github.com/schulterklopfer/cyphernode_admin/handlers"
+  "github.com/schulterklopfer/cyphernode_admin/models"
 )
 
 type Config struct {
@@ -42,8 +43,48 @@ func (app *App) Init() {
 	}
 }
 
-func (app *App) initContent() {
+func (self *App) initContent() {
 	// Create user id=1, app id=1, role id=1
+  role := new(models.RoleModel)
+  app := new(models.AppModel)
+  user := new(models.UserModel)
+
+  db := dataSource.GetDB()
+
+  db.First( role, 1 )
+  db.First( app, 1 )
+  db.First( user, 1 )
+
+  if !db.NewRecord( role ) || !db.NewRecord( app ) || !db.NewRecord( user ) {
+    return
+  }
+
+  role.ID = 1
+  role.Name = "admin"
+  role.Description = "Main admin with god mode"
+  role.AutoAssign = false
+  role.AppId = 1
+
+  roles := make( []*models.RoleModel, 1 )
+  roles[0]=role
+
+  app.ID = 1
+  app.Name = "Cyphernode admin"
+  app.Description = "Manage your cyphernode"
+  app.Hash = "adminapphash" // change me
+  app.AvailableRoles = roles
+
+  user.ID = 1
+  user.Login = self.config.InitialAdminUsername
+  user.Password = self.config.InitialAdminPassword
+  user.Name = "Adnim Administer"
+  user.EmailAddress = "admin@admin.rocks"
+  user.Roles = roles
+
+
+  db.Create( role )
+  db.Create( app )
+  db.Create( user )
 
 }
 
