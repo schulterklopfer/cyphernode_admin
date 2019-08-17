e949c0c54a716de9dfecea987200b8f236de9ea7
SKP <jash@schulterklopfer-productions.de>
2019-08-18 01:07:38 +0200

    meh! -.- !!

--- models/user.go
+++ models/user.go
@@ -3,10 +3,10 @@ package models
 import "github.com/jinzhu/gorm"
 
 type UserModel struct {
-	gorm.Model
-	Login string `json:"login" gorm:"type:varchar(30);unique_index;not null"`
-	Name string `json:"name"`
-	Password string `json:"password" gorm:"type:varchar(128)"`
-	EmailAddress string `json:"emailAddress" gorm:"type:varchar(100);not null"`
-	Roles []*RoleModel `json:"roles" gorm:"many2many:user_roles;"`
+  gorm.Model
+  Login string `json:"login" gorm:"type:varchar(30);unique_index;not null"`
+  Name string `json:"name"`
+  Password string `json:"password" gorm:"type:varchar(128)"`
+  EmailAddress string `json:"emailAddress" gorm:"type:varchar(100);not null"`
+  Roles []*RoleModel `json:"roles" gorm:"many2many:user_roles;"`
 }
